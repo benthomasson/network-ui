3aed4e1a6526c841dd6898091676ee9ddcac20e3
Ben Thomasson <bthomass@redhat.com>
2018-11-02 12:46:55 -0400

    Changes debug ref

--- network_ui_react_frontend/src/SVGFrame.js
+++ network_ui_react_frontend/src/SVGFrame.js
@@ -25,7 +25,7 @@ class SVGFrame extends Component {
 
   constructor(props) {
     super(props);
-    window.scope = this;
+    window.svgFrame = this;
     this.scope = new models.ApplicationScope(this);
   }
 
